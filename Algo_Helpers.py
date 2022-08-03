b7bcfb84a9b209c169295c26846cab1f97ce7463
DParkKeebeck <duncan.park@keebeckalpha.com>
2022-08-03 17:53:56 -0400

    8/3

--- Algo_Helpers.py
+++ Algo_Helpers.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from IPython.display import display
 import bt
+import numpy as np
+
+import matplotlib.pyplot as plt
+
 
 
 class Signal(bt.Algo):
@@ -30,9 +34,11 @@ class Signal(bt.Algo):
         else:
             target.temp['Signal'] = 0
         """
+        #display("here")
+
+        target.temp['Signal'] = {}
 
         for selected in target.perm['tickers']:
-            #display("sig: " + selected)
 
             t0 = target.now - self.lag
 
@@ -43,10 +49,14 @@ class Signal(bt.Algo):
             trend = prc.iloc[-1]/prc.iloc[0] - 1
             signal = trend > 0.
 
+            #display("sig: " + selected)
+
+            #target.temp['Signal'] = {selected: 0}
+
             if signal:
-                target.temp[selected] = 1
+                target.temp['Signal'][selected] = .2
             else:
-                target.temp[selected] = 0
+                target.temp['Signal'][selected] = 0
 
         return True
 
@@ -65,14 +75,74 @@ class WeighFromSignal(bt.Algo):
 
         target.temp['weights'] = {selected : target.temp['Signal']}
         return True"""
-        #display(target.perm['tickers'])
-        target.temp['weights'] = {'aapl': target.temp['aapl']}
+        #display(target.temp.items())
+
+        #target.temp['weights'] = {'aapl': target.temp['Signal']['aapl']}
+        target.temp['weights'] = {}
         for selected in target.perm['tickers']:
-            if target.temp[selected] is None:
+            if target.temp['Signal'][selected] is None:
                 raise(Exception('No Signal!'))
 
-            target.temp['weights'][selected] = target.temp[selected]
+            target.temp['weights'][selected] = target.temp['Signal'][selected]
+
+        #target.temp['cash'] = 0.4
+        return True
+
+class Rebalance(bt.Algo):
+
+    def __init__(self):
+        super(Rebalance, self).__init__()
+
+    def __call__(self, target):
 
 
-        display(target.temp['weights'])
+        if "weights" not in target.temp:
+            return True
+
+        targets = target.temp["weights"]
+
+        # save value because it will change after each call to allocate
+        # use it as base in rebalance calls
+        # call it before de-allocation so that notional_value is correct
+        if target.fixed_income:
+            if "notional_value" in target.temp:
+                base = target.temp["notional_value"]
+            else:
+                base = target.notional_value
+        else:
+            base = target.value
+
+        # de-allocate children that are not in targets and have non-zero value
+        # (open positions)
+        for cname in target.children:
+            # if this child is in our targets, we don't want to close it out
+            if cname in targets:
+                continue
+
+            # get child and value
+            c = target.children[cname]
+            if target.fixed_income:
+                v = c.notional_value
+            else:
+                v = c.value
+
+            # if non-zero and non-null, we need to close it out
+            if v != 0.0 and not np.isnan(v):
+                target.close(cname, update=False)
+
+        # If cash is set (it should be a value between 0-1 representing the
+        # proportion of cash to keep), calculate the new 'base'
+        if "cash" in target.temp and not target.fixed_income:
+            base = base * (1 - target.temp["cash"])
+
+        # Turn off updating while we rebalance each child
+        
+
+        for item in targets.items():
+            #display(base)
+            target.rebalance(item[1], child=item[0], base=base, update=False)
+
+        # Now update
+        target.root.update(target.now)
+
         return True
---
+++ Untitled-1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import matplotlib
+import numpy as np
+import pandas as pd
+import bt
+from IPython.display import display
+import matplotlib.pyplot as plt
+from Algo_Helpers import Signal, WeighFromSignal, Rebalance
+matplotlib.use( 'tkagg' )
+
+start_date = '2010-11-01'
+
+
+pdf = bt.get('aapl,msft,c,ge,gs', start=start_date)
+
+
+
+runMonthlyAlgo = bt.algos.RunWeekly()
+rebalAlgo = Rebalance()
+
+signalAlgo = Signal(pd.DateOffset(months=2),pd.DateOffset(days=1))
+
+weighFromSignalAlgo = WeighFromSignal()
+
+s = bt.Strategy(
+    'example1',
+    [
+        runMonthlyAlgo,
+        signalAlgo,
+        weighFromSignalAlgo,
+        rebalAlgo
+    ]
+)
+s.perm['tickers'] = list(pdf.columns)
+t = bt.Backtest(s, pdf, integer_positions=False, progress_bar=True)
+res = bt.run(t)
+
+
+plt.plot(res.prices)
+#pr = res.prices
+
+
+res.plot_security_weights()
+#res.prices.tail()
+#temp = res.get_security_weights()
+#test = res.get_transactions()
+#q = t.positions
+
+
+df = pdf['aapl']
+
+tmt = res.get_security_weights()['aapl']
+tmt = tmt.rename("weight")
+tmt = tmt.drop(tmt.index[0])
+rdf = pd.concat([df, tmt], axis=1)
+rdf['label'] = np.where(rdf['weight'] == 0, -1, 1)
+
+
+fig, ax = plt.subplots()
+fig.set_size_inches(10,8)
+def plot_func(group):
+    global ax
+    color = 'r' if (group['label'] < 0).all() else 'g'
+    lw = 1
+    ax.plot(group.index, group.aapl, c=color, linewidth=lw)
+
+
+rdf.groupby((rdf['label'].shift() * rdf['label'] < 0).cumsum()).apply(plot_func)
+
+plt.show()
+
+#fig.savefig('demo.png', bbox_inches='tight')
+
+
+
+res.stats
+
+
+
